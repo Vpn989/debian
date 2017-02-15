fcf2da8f5662d0efba30b03edeee058854648923
anggasa <anqghasiyaputra@hotmail.com>
2017-02-15 15:02:52 +0700

    Update user-login.sh

--- user-login.sh
+++ user-login.sh
@@ -1,67 +1,36 @@
 #!/bin/bash
-#
-# Script Copyright Worm
-# ==========================
-# 
+# Script by Worm
+
 
 data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
 
-echo "-----------------------";
-echo "Checking Dropbear login";
-echo "-----------------------";
+echo "Checking User Login";
+echo "=================================";
 
 for PID in "${data[@]}"
 do
-	#echo "check $PID";
-	NUM=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
-	USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
-	IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
-	if [ $NUM -eq 1 ]; then
-		echo "$PID - $USER - $IP";
-	fi
+    #echo "check $PID";
+    NUM=`cat /var/log/secure | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
+    USER=`cat /var/log/secure | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
+    IP=`cat /var/log/secure | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
+    if [ $NUM -eq 1 ]; then
+        echo "$PID - $USER - $IP";
+    fi
 done
-
-echo "";
+echo "---";
 
 data=( `ps aux | grep "\[priv\]" | sort -k 72 | awk '{print $2}'`);
 
-echo "----------------------";
-echo "Checking OpenSSH login";
-echo "----------------------";
 
 for PID in "${data[@]}"
 do
         #echo "check $PID";
-	NUM=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | wc -l`;
-	USER=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $9}'`;
-	IP=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $11}'`;
+        NUM=`cat /var/log/secure | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | wc -l`;
+        USER=`cat /var/log/secure | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $9}'`;
+        IP=`cat /var/log/secure | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $11}'`;
         if [ $NUM -eq 1 ]; then
                 echo "$PID - $USER - $IP";
         fi
 done
-
-echo "";
-
-echo "-------------------";
-echo "Checking PPTP login";
-echo "-------------------";
-last | grep ppp | grep still
-
-echo "";
-
-#Melihat Riwayat Login User
-echo "---------------------------";
-echo "Checking PPTP Login History";
-echo "---------------------------";
-last | grep ppp
-
-echo "";
-
-echo "------------------------------------------------"
-echo " Multi Login = Kill "
-echo " Cara pakai : Kill nomor PID "
-echo "------------------------------------------------"
-
-echo "";
-
-echo "Script By \e[1;33;44mWorm\e[0m"
+echo "-------------------------------"
+echo -e "Script by \e[1;33;44m Worm \e[0m"
